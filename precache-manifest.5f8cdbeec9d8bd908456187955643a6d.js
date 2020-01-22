f4f09b49276458f4c823523723b9003e853a3ea9
limabean <lditom808@gmail.com>
2020-01-22 14:35:43 -0500

    Updates

---
+++ precache-manifest.5f8cdbeec9d8bd908456187955643a6d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282e2f6664841069e4c75ece2fb05687",
+    "url": "/github.io/clicky_game/index.html"
+  },
+  {
+    "revision": "a1de1291a5a5ab694743",
+    "url": "/github.io/clicky_game/static/css/main.70e8c344.chunk.css"
+  },
+  {
+    "revision": "82cebd818bffd951d672",
+    "url": "/github.io/clicky_game/static/js/2.1e6cfe00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/clicky_game/static/js/2.1e6cfe00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1de1291a5a5ab694743",
+    "url": "/github.io/clicky_game/static/js/main.b3cf2ee8.chunk.js"
+  },
+  {
+    "revision": "6c42cbf21870c28e66ec",
+    "url": "/github.io/clicky_game/static/js/runtime-main.dc33c5f2.js"
+  },
+  {
+    "revision": "041a49a566ad2c9ba49803ed773eef4a",
+    "url": "/github.io/clicky_game/static/media/dog.041a49a5.jpg"
+  },
+  {
+    "revision": "4e2840d95c6734cc1a7f86ec3d1cc8f9",
+    "url": "/github.io/clicky_game/static/media/hedgehog.4e2840d9.jpg"
+  },
+  {
+    "revision": "e35c0a05173176647468a75554d94fb5",
+    "url": "/github.io/clicky_game/static/media/peacock.e35c0a05.jpg"
+  },
+  {
+    "revision": "fc89047499d7b1e575bac8c5d8a830bd",
+    "url": "/github.io/clicky_game/static/media/tiger.fc890474.jpg"
+  },
+  {
+    "revision": "a3c82f840f0c84c88a02e2f88068c876",
+    "url": "/github.io/clicky_game/static/media/whitetiger.a3c82f84.jpg"
+  },
+  {
+    "revision": "aeefafdbb7cffd711d3dea9960f99c29",
+    "url": "/github.io/clicky_game/static/media/zebra.aeefafdb.jpg"
+  }
+]);
